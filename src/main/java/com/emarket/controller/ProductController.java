4c66cfbcbcdf7eed38461f0ed916029074632931
Vladislav Merkodanov <vladislavik259@gmail.com>
2025-03-19 10:55:47 +0400

    add getProduct handle

--- src/main/java/com/emarket/controller/ProductController.java
+++ src/main/java/com/emarket/controller/ProductController.java
@@ -1,26 +1,25 @@
 package com.emarket.controller;
 
 import com.emarket.model.Product;
-import com.emarket.repository.ProductRepository;
+import com.emarket.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Date;
 
 @RestController
 public class ProductController {
-    final ProductRepository productRepository;
+    private final ProductService productService;
 
-    public ProductController(ProductRepository productRepository) {
-        this.productRepository = productRepository;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
 
-    @GetMapping("/")
-    public ResponseEntity<String> getProduct() {
-        Product product = new Product(1, 1, 1, new Date(), "a", "a");
-        productRepository.save(product);
-        return ResponseEntity.status(HttpStatus.CREATED).body("A");
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable long id) {
+        Product product = productService.findProductById(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(product);
     }
 }
