63c3d74613c851f64567d90e806360430f9d64cf
Vladislav Merkodanov <vladislavik259@gmail.com>
2025-03-23 16:46:52 +0400

    add swagger

--- src/main/java/com/emarket/controller/ClothingController.java
+++ src/main/java/com/emarket/controller/ClothingController.java
@@ -2,9 +2,13 @@ package com.emarket.controller;
 
 import com.emarket.dto.ClothingResponseDto;
 import com.emarket.mapper.ClothingMapper;
-import com.emarket.service.ClothingService;
 import com.emarket.model.Clothing;
+import com.emarket.service.ClothingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/clothing")
+@Tag(name = "Clothing controller",
+        description = "Контроллер управления одеждой")
+@RequiredArgsConstructor
 public class ClothingController {
     private final ClothingService clothingService;
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Поиск одежды по id", responses = {
+            @ApiResponse(responseCode = "200", description = "Одежда найдена"),
+            @ApiResponse(responseCode = "404", description = "Одежда не найдена")
+    })
     public ResponseEntity<ClothingResponseDto> getClothingResponseDto(@PathVariable long id) {
         Optional<Clothing> clothing = clothingService.findById(id);
         if (clothing.isEmpty()) {
