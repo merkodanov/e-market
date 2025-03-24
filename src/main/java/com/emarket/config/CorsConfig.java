250ae5b025e7e11b44e9f4b30e3b7789db8a277f
Vladislav Merkodanov <vladislavik259@gmail.com>
2025-03-24 10:23:45 +0400

    add config

--- src/main/java/com/emarket/config/CorsConfig.java
+++ src/main/java/com/emarket/config/CorsConfig.java
@@ -2,22 +2,31 @@ package com.emarket.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-
 import java.util.List;
 
 @Configuration(proxyBeanMethods = false)
+@EnableWebSecurity
 public class CorsConfig {
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource)
+            throws Exception {
+        return http.cors(
+                cors -> cors.configurationSource(corsConfigurationSource)).build();
+    }
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedHeader("Access-Control-Allow-Origin");
-        config.addAllowedHeader(HttpHeaders.CONTENT_TYPE);
+        config.addAllowedHeader("*");
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "HEAD", "DELETE", "OPTIONS"));
         config.setAllowedOrigins(List.of("localhost:5173", "127.0.0.1:5173"));
         config.setAllowCredentials(true);
